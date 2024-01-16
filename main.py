76da3bc62c8716e8b5915ba147fc018e1d4043c9
F8teTrue <preben.dahl2006@gmail.com>
2024-01-16 19:02:26 +0100

    Messaging version 1
    Can host and connect locally on the same computer in different terminals. Can then send messages between host and client.

--- main.py
+++ main.py
@@ -1,1 +1,54 @@
-print("Hello world!")
+import socket
+import subprocess
+import sys
+import threading
+
+def recieve_messages(sock):
+    while True:
+        data, addr = sock.recvfrom(1024)
+        data = data.decode("utf-8")
+        print(f'Recieved from {addr}: {data}')
+
+def server():
+    host = "127.0.0.1"
+    port = 4000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+
+    print("Server started")
+
+    recieve_thread = threading.Thread(target = recieve_messages, args = (s,))
+    recieve_thread.start()
+
+    while True:
+        message = input("-> ")
+        s.sendto(message.encode('utf-8'), ('127.0.0.1', 4005))
+
+def client():
+    host = "127.0.0.1"
+    port = 4005
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+
+    recieve_thread = threading.Thread(target = recieve_messages, args = (s,))
+    recieve_thread.start()
+
+    while True:
+        message = input("-> ")
+        s.sendto(message.encode('utf-8'), ('127.0.0.1', 4000))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python Main.py host|client")
+        sys.exit(1)
+
+    if sys.argv[1] == 'host':
+        server()
+    elif sys.argv[1] == 'client':
+        client()
+    else:
+        print("Invalid argument. Use 'host' or 'client'.")
+
+
