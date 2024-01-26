8b5ebac6f53531acad1d69e4befc4c9def362aaa
F8teTrue <preben.dahl2006@gmail.com>
2024-01-26 14:56:06 +0100

    Some comments

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import threading
 import rsa
 
 def receive_messages(sock, sender):
+    """
+    Listens continiously for data on the socket, then decodes it and prints it.
+    """
     while True:
         try:
-            data = sock.recv(1024)
+            data = sock.recv(1024) # This is listening for incomming data on the socket
             if not data:
                 print(f'Connection with {sender} closed.')
                 break
             data = data.decode("utf-8")
             print(f'Received from {sender}: {data}')
-        except OSError as e:
+
+            # If an error occurs the socket is closed.
+        except OSError as e: 
             if e.errno == 9:
                 print("Connection closed.")
                 break
@@ -20,6 +25,9 @@ def receive_messages(sock, sender):
     sock.close()
 
 def send_messages(sock):
+    """
+    Takes user input and sends it to the connected socket.
+    """
     while True:
         try:
             message = input("-> ")
@@ -28,6 +36,8 @@ def send_messages(sock):
                 sock.close()
                 break
             sock.send(message.encode('utf-8'))
+
+            # Same again. If an error occurs the socket is closed.
         except OSError as e:
             if e.errno == 9:
                 print("Connection closed.")
@@ -38,7 +48,11 @@ def send_messages(sock):
 
 hosting = input("Do you want to host (1) or connect (2): ")
 
+
 if hosting == "1":
+# The host creates a socket and binds it to an IP (any in this case) and a port.
+# It then listens for incomming connections and then wait for a client to connect and returns a new socket
+
     host = "0.0.0.0"
     port = 4000
 
@@ -53,27 +67,32 @@ if hosting == "1":
 
     c, _ = s.accept()
 
+# This starts 2 threads, 1 for recieving messages and one for sending messages.
     receive_thread = threading.Thread(target=receive_messages, args=(c, "client"))
     send_thread = threading.Thread(target=send_messages, args=(c,))
 
     receive_thread.start()
     send_thread.start()
 
+# Waits for both threads to finish and closes the sockets after.
     receive_thread.join()
     send_thread.join()
     c.close()
     s.close()
 
 elif hosting == "2":
-    client = "10.58.176.2"
+# The client creates a socket and connects to the host using the hosts IP and the chosen port.
+
+    server = "10.58.176.2"
     port = 4000
 
     c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c.connect((client, port))
+    c.connect((server, port))
 
     print("Connected to host.")
     print('"exit" to close.')
 
+# Same as the host with starting the 2 threads for recieving and sending messages.
     receive_thread = threading.Thread(target=receive_messages, args=(c, "host"))
     send_thread = threading.Thread(target=send_messages, args=(c,))
 
